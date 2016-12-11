610072b8c4a0af3189d41f0224e9fb6e4f4c36d0
shubh24 <shubhankar24@gmail.com>
2016-12-11 16:38:26 +0530

    shifted to ggmap

--- gif_terror.R
+++ gif_terror.R
@@ -1,12 +1,12 @@
-library(sp)
-library(raster)
+library(ggplot2)
+library(ggmap)
 library(animation)
 
 df = read.csv("globalterrorismdb_0616dist.csv", stringsAsFactors = TRUE)
 
-generate_gif = function(start_year, country_code, country){
+generate_gif = function(start_year, zoom_val, country){
   
-  country_map <- getData('GADM', country=country_code, level=1)
+  country_map = get_map(country, zoom = zoom_val, maptype = "toner-lite")
   country_df = df[df$country_txt == country,]
 
   i = start_year
@@ -17,20 +17,15 @@ generate_gif = function(start_year, country_code, country){
     
     cities <- data.frame(lon=country_year$lon, lat=country_year$lat)
     
-    print( spplot(country_map, "NAME_1",
-                  sp.layout=list("panel.points", cities$lon, cities$lat, col="red"),
-                  main=paste(country, " -- Terrorist Incidents in ",i, sep = ""),
-                  colorkey=FALSE,
-                  scales=list(draw=TRUE)
-    ))
-    
+    print(ggmap(country_map)+geom_point(aes(x = lon, y = lat), data = cities, col = "red", alpha = .5)+ggtitle(paste(country, "-- Terror Strikes in ", i, sep="")))
+
     i = i+1
     
   }, movie.name = paste(country, "_terror.gif", sep=""), interval = 3.5, convert = "convert", ani.width = 1000, ani.height = 800)
 
 }
 
-generate_gif(2010, "SYR", "Syria")
-generate_gif(2010, "AFG", "Afghanistan")
-generate_gif(2010, "IND", "India")
-generate_gif(2010, "GBR", "United Kingdom")
+generate_gif(2010, 6, "Syria")
+generate_gif(2010, 6, "Afghanistan")
+generate_gif(2010, 4, "India")
+generate_gif(2010, 5, "United Kingdom")
